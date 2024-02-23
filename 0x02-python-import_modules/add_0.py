84ec5e6046a57f64a23e1fb9b6379e422f740946
KARENKAMORI <kamorikaren2019@gmail.com>
2024-02-23 11:09:16 -0800

    import modules in python

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """addition function
+
+    Args:
+        a: integer one
+        b: integer two
+
+    Returns:
+        a + b return value
+    """
+    return (a + b)
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """function: addition
+
+    Args:
+        a: integer one
+        b: integer two
+
+    Returns:
+        a + b return value.
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """function: subtraction
+
+    Args:
+        a: integer one
+        b: integer two
+
+    Returns:
+        a - b return value
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """function: Multiplication
+
+    Args:
+        a: integer one
+        b: integer two
+
+    Returns:
+        a * b return value
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """function: Division
+
+    Args:
+        a: integer one
+        b: integer two
+
+    Returns:
+        a / b return value
+    """
+    return int(a / b)
