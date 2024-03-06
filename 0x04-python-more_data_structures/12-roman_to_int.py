2268b997d6170dda672a15a4db522c67e721bf3b
KARENKAMORI <kamorikaren2019@gmail.com>
2024-03-06 05:22:37 -0800

    Data structures: sets, Dictionaries

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     x = 0
 
     if type(roman_string) is str and roman_string:
         for char in range(len(roman_string) - 1, -1, -1):
-            if roman_value[roman_string[char]] >= x:
-                result += roman_value[roman_string[char]]
+            if roman[roman_string[char]] >= x:
+                result += roman[roman_string[char]]
             else:
-                result -= roman_value[roman_string[char]]
-            x = roman_value[roman_string[char]]
+                result -= roman[roman_string[char]]
+            x = roman[roman_string[char]]
     return result
