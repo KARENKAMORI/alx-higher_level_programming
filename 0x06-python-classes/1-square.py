a3180fc880dab04b15d9028803b9ae438119d7b8
KARENKAMORI <kamorikaren2019@gmail.com>
2024-03-20 04:52:13 -0700

    a class Square that defines a square by: (based on 0-square.py)
    Private instance attribute: size
    Instantiation with size (no type/value verification)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class Square defining a square"""
+
+class Square:
+    """ class Square defining a square"""
+    def __init__(self, size):
+        """square initialization
+        Args:
+            size (int): square size
+        """
+        self.__size = size  #: square size
