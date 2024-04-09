eed446a4cc6dd632630acdac1d858a085bf947a2
KAMORIKAREN <kamorikaren2019@gmail.com>
2024-04-09 12:32:07 -0700

    rectangle operations

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle():
         rectangle = ""
 
         if self.__width > 0 and self.__height > 0:
-            for y in range(self.__height):
+            for i in range(self.__height):
                 rectangle += '#' * self.__width + '\n'
 
         return rectangle[:-1]
