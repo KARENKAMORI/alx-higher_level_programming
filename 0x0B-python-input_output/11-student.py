403cdf0d0bf72b2f24853990c9210dcd7af80ade
KAMORIKAREN <kamorikaren2019@gmail.com>
2024-04-16 19:51:58 -0700

    a class Student that defines a student by: (based on 10-student.py)
    Public instance attributes:
    first_name
    last_name
    age
    Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
    Public method def to_json(self, attrs=None): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py):
    If attrs is a list of strings, only attributes name contain in this list must be retrieved.
    Otherwise, all attributes must be retrieved
    Public method def reload_from_json(self, json): that replaces all attributes of the Student instance:
    You can assume json will always be a dictionary
    A dictionary key will be the public attribute name
    A dictionary value will be the value of the public attribute
    You are not allowed to import any module

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module: Student.
+
+Has a Student class and methods.
+"""
+
+
+class Student():
+    """Class defines a Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Sets the Student object necessary attributes.
+
+        Args:
+            first_name (str): student first name.
+            last_name (str): student last name.
+            age (int): Student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a Student instance dictionary representation."""
+        if attrs is not None:
+            return {x: y for x, y in self.__dict__.items() if x in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all Student instance attributes."""
+        for x, y in json.items():
+            setattr(self, x, y)
